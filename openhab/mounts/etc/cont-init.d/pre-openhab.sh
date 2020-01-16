7f093e50bf805d1a52570715dba825283b28f5f4
Jan Wolf <mail@jan-wolf.de>
2020-01-16 22:14:13 +0100

    Overwrite volume attached files

---
+++ openhab/mounts/etc/cont-init.d/pre-openhab.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+if [ ! -z ${AUTH_OPENHAB_UUID} ]
+then
+    mkdir -p /openhab/userdata
+    echo ${AUTH_OPENHAB_UUID} > /openhab/userdata/uuid
+fi
+if [ ! -z ${AUTH_OPENHAB_SECRET} ]
+then
+    mkdir -p /openhab/userdata/openhabcloud
+    echo ${AUTH_OPENHAB_SECRET} > /openhab/userdata/openhabcloud/secret
+fi
+
+echo ${AUTH_DEVICE_HOSTKEY} > /openhab/userdata/etc/host.key
+
+sed -i "s?AUTH_INFLUXDB_PASSWORD?${AUTH_INFLUXDB_PASSWORD}?g" /tmp/openhab/userdata/config/org/openhab/influxdb.config
+cp /tmp/openhab/userdata/config/org/openhab/influxdb.config /openhab/userdata/config/org/openhab/influxdb.config
+
+filename=/openhab/userdata/config/org/openhab/addons.config
+if [ ! -f $filename ]
+then
+    cp /tmp/openhab/userdata/config/org/openhab/addons.config.template /openhab/userdata/config/org/openhab/addons.config
+fi
