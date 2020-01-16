eb42d7e40577398d4e6fb82114bf90edc8c62b8f
Jan Wolf <mail@jan-wolf.de>
2020-01-16 20:11:18 +0100

    debug traefik

--- traefik/pre-traefik.sh
+++ traefik/pre-traefik.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 HASHED=$(openssl passwd -1 ${AUTH_OPENHAB_PASSWORD})
-sed -i "s/AUTH_OPENHAB_PASSWORD/${HASHED}/g" /etc/traefik/traefik.toml
+sed -i "s?AUTH_OPENHAB_PASSWORD?${HASHED}?g" /etc/traefik/traefik.toml
 #exec /traefik "$@"
