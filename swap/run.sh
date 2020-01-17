6f8c0c048d0440c8d8a65046c19e754595ecd5d3
Jan Wolf <mail@jan-wolf.de>
2020-01-17 19:45:15 +0100

    debug swap

--- swap/run.sh
+++ swap/run.sh
@@ -1,27 +1,8 @@
 #!/bin/sh
-if [ "${SWAP_SIZE}" != "**None**"  ]; then
-    echo "=> Creating ssh access"
-    mkdir -p /user/.ssh
-    chmod 700 /user/.ssh
-    if [ ! -f /user/.ssh/id_docker ]; then
-        echo -e "\n\n\n" | ssh-keygen -N "" -f /user/.ssh/id_docker
-        echo "=> Updating Authorized Keys"
-        touch /user/.ssh/authorized_keys
-        chmod 600 /user/.ssh/authorized_keys
-        cat /user/.ssh/id_docker.pub >> /user/.ssh/authorized_keys
-    fi
 
-    DOCKER_HOST=$(/sbin/ip route|awk '/default/ { print $3  }')
-    echo "=> Creating swap on ${DOCKER_HOST}"
-
-    ssh root@$DOCKER_HOST -o "StrictHostKeyChecking=no" -i /user/.ssh/id_docker fallocate -l ${SWAP_SIZE}MB /swapfile
-    ssh root@$DOCKER_HOST -o "StrictHostKeyChecking=no" -i /user/.ssh/id_docker chmod 600 /swapfile
-    ssh root@$DOCKER_HOST -o "StrictHostKeyChecking=no" -i /user/.ssh/id_docker mkswap /swapfile
-    ssh root@$DOCKER_HOST -o "StrictHostKeyChecking=no" -i /user/.ssh/id_docker swapon /swapfile
-
-    echo "=> Setting swappiness on ${DOCKER_HOST}"
-    ssh root@$DOCKER_HOST -o "StrictHostKeyChecking=no" -i /user/.ssh/id_docker sysctl vm.swappiness=${SWAPPINESS}
-else
-    echo "ERROR: No swap size found in \$SWAP_SIZE"
-    exit 1
+if grep -q -F /`basename $SWAPFILE` /proc/swaps; then
+   echo "swap file already present"
+   exit
 fi
+
+mkdir -p `dirname $SWAPFILE` && dd if=/dev/zero of=$SWAPFILE bs=1024k count=$SWAPSIZE && mkswap $SWAPFILE && chmod 0600 $SWAPFILE && swapon $SWAPFILE && echo "$SWAPFILE none swap sw 0 0" >> /etc/fstab && echo "created swap file"
