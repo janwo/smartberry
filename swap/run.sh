a0085fb31fa2edebd95fade4c1daf8615ce84478
Jan Wolf <mail@jan-wolf.de>
2020-01-17 20:06:55 +0100

    Remove swap

--- swap/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if grep -q -F /`basename $SWAPFILE` /proc/swaps; then
-   echo "swap file already present"
-   exit
-fi
-
-mkdir -p `dirname $SWAPFILE` && dd if=/dev/zero of=$SWAPFILE bs=1024k count=$SWAPSIZE && mkswap $SWAPFILE && chmod 0600 $SWAPFILE && swapon $SWAPFILE && echo "$SWAPFILE none swap sw 0 0" >> /etc/fstab && echo "created swap file"
