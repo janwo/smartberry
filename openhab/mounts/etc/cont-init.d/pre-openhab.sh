3320c790c7357d4c785410e14fa08c960a2c835f
Jan Wolf <mail@jan-wolf.de>
2020-01-17 15:16:17 +0100

    Fix find cmd

--- openhab/mounts/etc/cont-init.d/pre-openhab.sh
+++ openhab/mounts/etc/cont-init.d/pre-openhab.sh
@@ -18,11 +18,11 @@ echo ${AUTH_DEVICE_HOSTKEY} > /openhab/userdata/etc/host.key
 sed -i "s?AUTH_INFLUXDB_PASSWORD?${AUTH_INFLUXDB_PASSWORD}?g" /tmp/openhab/userdata/config/org/openhab/influxdb.config
 
 # Remove non standard files within conf-folder
-find /openhab/conf/sitemaps ! -name 'customsitemap-*.sitemap' ! -name 'home.sitemap' ! -name 'readme.txt' -delete
-find /openhab/conf/items ! -name 'customitem-*.items' ! -name 'readme.txt' -delete
-find /openhab/conf/scripts ! -name 'customscript-*.py' ! -name 'readme.txt' -delete
-find /openhab/conf/rules ! -name 'customrule-*.rules' ! -name 'readme.txt' -delete
-find /openhab/conf/transform ! -name 'customtransform-*.map' ! -name 'readme.txt' -delete
+find /openhab/conf/sitemaps -type f ! -name 'customsitemap-*.sitemap' ! -name 'home.sitemap' ! -name 'readme.txt' -delete
+find /openhab/conf/items -type f ! -name 'customitem-*.items' ! -name 'readme.txt' -delete
+find /openhab/conf/scripts -type f ! -name 'customscript-*.py' ! -name 'readme.txt' -delete
+find /openhab/conf/rules -type f ! -name 'customrule-*.rules' ! -name 'readme.txt' -delete
+find /openhab/conf/transform -type f ! -name 'customtransform-*.map' ! -name 'readme.txt' -delete
 
 # Overwrite files including conf-folder
 find /tmp/openhab -type f -exec cp -t /openhab {} +
