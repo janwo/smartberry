b0ba9bc565396abe668833296e941edf83add934
Jan Wolf <mail@jan-wolf.de>
2020-01-22 22:54:52 +0100

    remove argument

--- samba/pre-samba.sh
+++ samba/pre-samba.sh
@@ -2,4 +2,4 @@
 export USER0="openhab;${AUTH_SAMBA_PASSWORD};9001;openhab;9001"
 export USER1="grafana;${AUTH_SAMBA_PASSWORD};472;grafana;472"
 export USER2="influxdb;${AUTH_SAMBA_PASSWORD}"
-exec /usr/bin/samba.sh "$@"
+exec /usr/bin/samba.sh
