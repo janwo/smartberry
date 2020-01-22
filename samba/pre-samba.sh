fce42b70dab5727072d919ed6323afc570121372
Jan Wolf <mail@jan-wolf.de>
2020-01-22 22:42:04 +0100

    fix user

--- samba/pre-samba.sh
+++ samba/pre-samba.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export USER0="openhab;${AUTH_SAMBA_PASSWORD};9001;openhab;9001"
 export USER1="grafana;${AUTH_SAMBA_PASSWORD};472;grafana;472"
-export USER2="influxdb;${AUTH_SAMBA_PASSWORD};0;root;0"
+export USER2="influxdb;${AUTH_SAMBA_PASSWORD};1000;root;0"
 exec /usr/bin/samba.sh "$@"
