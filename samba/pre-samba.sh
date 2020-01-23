9e1c2a798bdf46098b68a037a7e8a10210f9cfa2
Jan Wolf <mail@jan-wolf.de>
2020-01-23 20:00:39 +0100

    debug samba

--- samba/pre-samba.sh
+++ samba/pre-samba.sh
@@ -2,8 +2,7 @@
 #export USER_2="influxdb;test"
 #export USER_1="openhab;test;9001;openhab;9001"
 #export USER_3="grafana;test;472;grafana;472"
-exec /usr/bin/samba.sh "$@  -u \"example1;badpass\" \
-            -u \"example2;badpass\" \
+exec /usr/bin/samba.sh "$@ \
             -s \"smartberry;/mount/openhab;yes;no;no;openhab\" \
             -s \"smartberry;/mount/grafana;yes;no;no;grafana\" \
             -s \"smartberry;/mount/influxdb;yes;no;no;influxdb\""
