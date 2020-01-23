61b0c33f3abcf1171dc6b108bb4e939cd9643758
Jan Wolf <mail@jan-wolf.de>
2020-01-23 20:09:13 +0100

    debug samba

--- samba/pre-samba.sh
+++ samba/pre-samba.sh
@@ -2,4 +2,7 @@
 export USER1="influxdb;${AUTH_SAMBA_PASSWORD}"
 export USER2="openhab;${AUTH_SAMBA_PASSWORD};9001;openhab;9001"
 export USER3="grafana;${AUTH_SAMBA_PASSWORD};472;grafana;472"
+echo $USER1
+echo $USER2
+echo $USER3
 exec /usr/bin/samba.sh "$@"
