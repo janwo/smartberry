63cf191ee221df9c05e25d221fcce7d82a1d1aef
Jan Wolf <mail@jan-wolf.de>
2020-01-23 20:57:49 +0100

    Restructure passwords

--- grafana/pre-grafana.sh
+++ grafana/pre-grafana.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export GF_SECURITY_ADMIN_PASSWORD=${AUTH_GRAFANA_PASSWORD}
+sed -i "s?DATASOURCE_INFLUXDB_USER_PASSWORD?${AUTH_INFLUXDB_PASSWORD}?g" /etc/grafana/provisioning/datasources.yml
 exec /run.sh "$@"
