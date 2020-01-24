d1a0885b139d127a2b487c86d4628f0500ea2c50
Jan Wolf <mail@jan-wolf.de>
2020-01-24 19:06:47 +0100

    some modifications

---
+++ openhab/mounts/etc/cont-init.d/00-jython.sh
@@ -0,0 +1,16 @@
+export EXTRA_JAVA_OPTS="${EXTRA_JAVA_OPTS} ${JYTHON_JAVA_OPTS}"
+find ${OPENHAB_HOME}/conf/automation/lib/python -name '*\$py.class' -delete
+
+## Ensure we are running new rule engine addon - is there a better way?
+MISC_LINE=$(grep '^[[:space:]]\?misc' ${OPENHAB_HOME}/conf/services/addons.cfg)
+if [ $? -eq 0 ]; then
+  ## ensure we have ruleengine enabled
+  if [[ ${MISC_LINE} == *"ruleengine"* ]]; then
+    echo "New rule engine is already included in the addons.cfg"
+  else
+    sed -i 's/misc\s\?=\s\?/misc = ruleengine,/' ${OPENHAB_HOME}/conf/services/addons.cfg
+  fi
+else
+  ## Just append last line
+  echo "misc = ruleengine" >> ${OPENHAB_HOME}/conf/services/addons.cfg
+fi
