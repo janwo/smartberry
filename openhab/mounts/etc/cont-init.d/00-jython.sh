08dcf5b11221b39b9553ba7095c3ded795d57a20
Jan Wolf <mail@jan-wolf.de>
2020-01-25 16:36:19 +0100

    fix init of personal package

--- openhab/mounts/etc/cont-init.d/00-jython.sh
+++ openhab/mounts/etc/cont-init.d/00-jython.sh
@@ -1,5 +1,5 @@
 export EXTRA_JAVA_OPTS="${EXTRA_JAVA_OPTS} ${JYTHON_JAVA_OPTS}"
 rsync -a /tmp/jython/automation ${OPENHAB_HOME}/conf
 mv ${OPENHAB_HOME}/conf/automation/lib/python/configuration.py.example ${OPENHAB_HOME}/conf/automation/lib/python/configuration.py
-mv ${OPENHAB_HOME}/conf/automation/lib/python/personal/__init.py.example ${OPENHAB_HOME}/conf/automation/lib/python/personal/__init.py
+mv ${OPENHAB_HOME}/conf/automation/lib/python/personal/__init__.py.example ${OPENHAB_HOME}/conf/automation/lib/python/personal/__init__.py
 find ${OPENHAB_HOME}/conf/automation/lib/python -name '*\$py.class' -delete
