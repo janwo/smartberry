574414291b6b64ac5b687c7cdbaa5e0eaa557b83
Jan Wolf <mail@jan-wolf.de>
2020-01-26 18:04:36 +0100

    try to fix samba

--- samba/pre-samba.sh
+++ samba/pre-samba.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export USER="openhab;${AUTH_SAMBA_PASSWORD};9001;openhab;9001"
+export USER="openhab;${AUTH_SAMBA_PASSWORD}"
 #export USER1="influxdb;${AUTH_SAMBA_PASSWORD}"
 #export USER2="openhab;${AUTH_SAMBA_PASSWORD};9001;openhab;9001"
 #export USER3="grafana;${AUTH_SAMBA_PASSWORD};472;grafana;472"
