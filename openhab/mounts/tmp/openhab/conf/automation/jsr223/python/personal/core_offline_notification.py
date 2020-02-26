8fbb68bff283449fa2350427af38121d8da1f9c1
Jan Wolf <mail@jan-wolf.de>
2020-02-26 18:11:42 +0100

    Fix offline things

--- openhab/mounts/tmp/openhab/conf/automation/jsr223/python/personal/core_offline_notification.py
+++ openhab/mounts/tmp/openhab/conf/automation/jsr223/python/personal/core_offline_notification.py
@@ -2,7 +2,6 @@ import functools
 from core.triggers import when
 from core.rules import rule
 from personal.core_misc import BroadcastType, broadcast
-import functools
 
 
 @rule("Core - Check things for offline state.", description="Check things for offline state.", tags=[])
@@ -10,16 +9,17 @@ import functools
 def offline_check(event):
     offlineThingsNotifications = filter(lambda thing: (
         thing.getStatusInfo() != None and
-        not thing.getStatusInfo().getStatus().toString().equals("ONLINE")
-    ), things)
-
+        thing.getStatusInfo().getStatus().toString() != "ONLINE"
+    ), things.getAll())
     offlineThingsNotifications = map(lambda thing: ("{0} is {1}".format(
         thing.getLabel(), thing.getStatusInfo().getStatus()
     )), offlineThingsNotifications)
 
     if len(offlineThingsNotifications) > 0:
-        notification = "Es wurden Geräte als nicht ONLINE erkannt:\n{}".format(
-            functools.reduce("\n", offlineThingsNotifications))
+        notification = "Es wurden Geräte als nicht ONLINE erkannt:\n{}".format(functools.reduce(
+            lambda a, b: a + "\n" + b,
+            offlineThingsNotifications
+        ))
         broadcast(notification)
     else:
         offline_check.log.info("All things are ONLINE!")
