969aff3d06ed9c98073531c7f6e25bd133de1fbc
Jan Wolf <mail@jan-wolf.de>
2020-04-21 11:17:09 +0200

    remove grafana

--- samba/pre-samba.sh
+++ samba/pre-samba.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 export USER="openhab;${AUTH_SAMBA_PASSWORD}"
-#export USER1="influxdb;${AUTH_SAMBA_PASSWORD}"
-#export USER2="openhab;${AUTH_SAMBA_PASSWORD};9001;openhab;9001"
-#export USER3="grafana;${AUTH_SAMBA_PASSWORD};472;grafana;472"
 exec /usr/bin/samba.sh "$@"
