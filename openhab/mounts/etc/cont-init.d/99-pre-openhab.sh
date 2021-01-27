7ae20fb367b5c97af14b29a06a41537141cd241d
Jan Wolf <mail@jan-wolf.de>
2021-01-27 14:10:15 +0100

    fix karaf replacement

--- openhab/mounts/etc/cont-init.d/99-pre-openhab.sh
+++ openhab/mounts/etc/cont-init.d/99-pre-openhab.sh
@@ -31,7 +31,7 @@ rsync -av -rv /tmp/${OPENHAB_HOME}/ ${OPENHAB_HOME}
 
 # Allow internal ssh connections
 RUNTIME_FILE=${OPENHAB_HOME}/conf/services/runtime.cfg
-sed -E -e 's/#?\s?(org.apache.karaf.shell:sshHost\s?=\s?)[^\s]*/\10.0.0.0/' ${RUNTIME_FILE}
+sed -n -e 's/[#\s]*\(org.apache.karaf.shell:sshHost\s\?=\s\?\)[^\s]*/\10.0.0.0/' ${RUNTIME_FILE}
 
 # Start addons.cfg transformations
 ADDONS_FILE=${OPENHAB_HOME}/conf/services/addons.cfg
@@ -40,7 +40,7 @@ ADDONS_FILE=${OPENHAB_HOME}/conf/services/addons.cfg
 TRANSFORMATION_LINE="$(grep -E '^[^#]?\s?transformation' ${ADDONS_FILE} || echo '' )"
 if [ "${TRANSFORMATION_LINE}" != '' ]; then
     if [[ "${TRANSFORMATION_LINE}" != *"map"* ]]; then
-        sed -E -e 's/transformation\s\?=\s\?/transformation = map,/' ${ADDONS_FILE}
+        sed -n -e 's/transformation\s\?=\s\?/transformation = map,/' ${ADDONS_FILE}
     fi
 else
     ## Just append last line
@@ -51,7 +51,7 @@ fi
 MISC_LINE="$(grep -E '^[^#]?\s?misc' ${ADDONS_FILE} || echo '' )"
 if [ "${MISC_LINE}" != '' ]; then
     if [[ "${MISC_LINE}" != *"openhabcloud"* ]]; then
-        sed -E -e 's/misc\s\?=\s\?/misc = openhabcloud,/' ${ADDONS_FILE}
+        sed -n -e 's/misc\s\?=\s\?/misc = openhabcloud,/' ${ADDONS_FILE}
     fi
 else
     ## Just append last line
@@ -62,7 +62,7 @@ fi
 AUTOMATION_LINE="$(grep -E '^[^#]?\s?automation' ${ADDONS_FILE} || echo '')"
 if [ "${AUTOMATION_LINE}" != '' ]; then
     if [[ "${AUTOMATION_LINE}" != *"jythonscripting"* ]]; then
-        sed -E -e 's/automation\s\?=\s\?/automation = jythonscripting,/' ${ADDONS_FILE}
+        sed -n -e 's/automation\s\?=\s\?/automation = jythonscripting,/' ${ADDONS_FILE}
     fi
 else
     ## Just append last line
@@ -73,7 +73,7 @@ fi
 PERSISTENCE_LINE="$(grep -E '^[^#]?\s?persistence' ${ADDONS_FILE} || echo '' )"
 if [ "${PERSISTENCE_LINE}" != '' ]; then
     if [[ "${PERSISTENCE_LINE}" != *"rrd4j"* ]]; then
-        sed -E -e 's/persistence\s\?=\s\?/persistence = rrd4j,/' ${ADDONS_FILE}
+        sed -n -e 's/persistence\s\?=\s\?/persistence = rrd4j,/' ${ADDONS_FILE}
     fi
 else
     ## Just append last line
