b6e5e800401166d39340dd07a4607012ee9064af
Jan Wolf <mail@jan-wolf.de>
2021-01-27 11:01:21 +0100

    Remove old python files

--- openhab/mounts/etc/cont-init.d/00-jython.sh
+++ openhab/mounts/etc/cont-init.d/00-jython.sh
@@ -3,11 +3,13 @@
 AUTOMATION_LIB_PYTHON_PATH=${OPENHAB_HOME}/conf/automation/lib/python
 AUTOMATION_JSR223_PYTHON_PATH=${OPENHAB_HOME}/conf/automation/jsr223/python
 if [ -d ${AUTOMATION_LIB_PYTHON_PATH} ]; then
-    find $AUTOMATION_LIB_PYTHON_PATH -type f -name '*\$py.class' -delete
-    find $AUTOMATION_LIB_PYTHON_PATH -type f -name '*\core_*.py' -delete
+    find $AUTOMATION_LIB_PYTHON_PATH/core/* -type f -exec rm -f {} +
+    find $AUTOMATION_LIB_PYTHON_PATH -type f -name '$py.class' -exec rm -f {} +
+    find $AUTOMATION_LIB_PYTHON_PATH -type f -name 'core_*.py' -exec rm -f {} +
 fi
 
 if [ -d ${AUTOMATION_JSR223_PYTHON_PATH} ]; then
-    find $AUTOMATION_JSR223_PYTHON_PATH -type f -name '*\$py.class' -delete
-    find $AUTOMATION_JSR223_PYTHON_PATH -type f -name '*\core_*.py' -delete
+    find $AUTOMATION_JSR223_PYTHON_PATH/core/* -type f -exec rm -f {} +
+    find $AUTOMATION_JSR223_PYTHON_PATH -type f -name '$py.class' -exec rm -f {} +
+    find $AUTOMATION_JSR223_PYTHON_PATH -type f -name 'core_*.py' -exec rm -f {} +
 fi
