2b6b321e2055abfcd6e59caeb7c38b6a8c46d5c9
Jan Wolf <mail@jan-wolf.de>
2021-01-28 18:44:00 +0100

    use absolute path

--- openhab/mounts/etc/cont-init.d/30-pages.sh
+++ openhab/mounts/etc/cont-init.d/30-pages.sh
@@ -2,7 +2,7 @@
 
 TARGET_JSON=${OPENHAB_HOME}/userdata/jsondb/uicomponents_ui_page.json
 TMP_JSON=${TARGET_JSON}.tmp
-REPLACEMENT_JSON=./~30-pages.json
+REPLACEMENT_JSON=/etc/cont-init.d/~30-pages.json
 
 jq --slurpfile REPLACEMENT ${REPLACEMENT_JSON} \
 '(del(.overview.value.slots.default[]|select(.config.title=="Einstellungen")).overview.value.slots.default+[$REPLACEMENT]) as $array|.overview.value.slots.default=$array' \
