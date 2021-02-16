14aaca604e4592222fb3f0194581687c2bf6566a
Jan Wolf <mail@jan-wolf.de>
2021-02-16 18:22:51 +0100

    Fix heater values as int

--- openhab/mounts/tmp/openhab/conf/automation/jsr223/python/personal/core_heating.py
+++ openhab/mounts/tmp/openhab/conf/automation/jsr223/python/personal/core_heating.py
@@ -12,7 +12,7 @@ from core.jsr223.scope import ir, UnDefType, events, OPEN
 def update_heater_on_contact_trigger(event):
     for member in ir.getItem("gCore_Heating_Thermostat_Mode").members:
         if any((contact.state == OPEN and has_same_location(contact, member)) for contact in ir.getItem("gCore_Heating_ContactSwitchable").members):
-            events.sendCommand(member, HeatingState.OFF)
+            events.sendCommand(member, int(HeatingState.OFF))
         else:
             state = ir.getItem("Core_Heating_Thermostat_ModeDefault").state
             events.sendCommand(member, state.intValue())
