79c565de9ea4a7308c8e43d7930c9909a092ecc0
Jan Wolf <mail@jan-wolf.de>
2021-03-08 21:00:13 +0100

    quick fix of reset-jython-directory.sh

--- openhab/mounts/etc/cont-init.d/20-prepare-target/reset-jython-directory.sh
+++ openhab/mounts/etc/cont-init.d/20-prepare-target/reset-jython-directory.sh
@@ -7,7 +7,6 @@ AUTOMATION_JSR223_PYTHON_PATH=${OPENHAB_HOME}/conf/automation/jsr223/python
 if [ -d ${AUTOMATION_LIB_PYTHON_PATH} ]; then
     echo "reset-jython-directory.sh: Reset old core files in ${AUTOMATION_LIB_PYTHON_PATH}..."
     find $AUTOMATION_LIB_PYTHON_PATH/core/* -type f -exec rm -f {} +
-    find $AUTOMATION_LIB_PYTHON_PATH/community/* -type f -exec rm -f {} +
     find $AUTOMATION_LIB_PYTHON_PATH -type f -name '$py.class' -exec rm -f {} +
     find $AUTOMATION_LIB_PYTHON_PATH -type f -name 'core_*.py' -exec rm -f {} +
 else
@@ -17,7 +16,6 @@ fi
 if [ -d ${AUTOMATION_JSR223_PYTHON_PATH} ]; then
     echo "reset-jython-directory.sh: Reset old core files in ${AUTOMATION_JSR223_PYTHON_PATH}..."
     find $AUTOMATION_JSR223_PYTHON_PATH/core/* -type f -exec rm -f {} +
-    find $AUTOMATION_JSR223_PYTHON_PATH/community/* -type f -exec rm -f {} +
     find $AUTOMATION_JSR223_PYTHON_PATH -type f -name '$py.class' -exec rm -f {} +
     find $AUTOMATION_JSR223_PYTHON_PATH -type f -name 'core_*.py' -exec rm -f {} +
 else
