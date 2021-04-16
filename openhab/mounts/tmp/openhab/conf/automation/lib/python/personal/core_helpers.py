7dce1839bb954b4ddd33e5875e10e2b9356075ed
Jan Wolf <mail@jan-wolf.de>
2021-04-16 17:21:21 +0200

    fix rules

--- openhab/mounts/tmp/openhab/conf/automation/lib/python/personal/core_helpers.py
+++ openhab/mounts/tmp/openhab/conf/automation/lib/python/personal/core_helpers.py
@@ -1,5 +1,4 @@
 from __future__ import unicode_literals
-from core.jsr223.scope import ruleRegistry
 from core.jsr223.scope import ir, scriptExtension
 from core.metadata import set_key_value, get_key_value, remove_key_value, get_value
 from core.items import add_item
@@ -13,6 +12,7 @@ from core.actions import Semantics
 from core.rules import rule
 from core.triggers import when
 scriptExtension.importPreset("RuleSupport")
+from core.jsr223.scope import ruleRegistry
 
 METADATA_NAMESPACE = "core"
 DATE_STRING = "yyyy-MM-dd'T'HH:mm:ss.SSxx"
