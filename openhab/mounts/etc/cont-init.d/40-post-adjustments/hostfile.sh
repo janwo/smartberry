83fc56b9db47a3da8e105bf3e96d6df85d58d318
Jan Wolf <mail@jan-wolf.de>
2021-04-21 22:15:35 +0200

    Rename AUTH_OPENHAB_HOSTKEY

--- openhab/mounts/etc/cont-init.d/40-post-adjustments/hostfile.sh
+++ openhab/mounts/etc/cont-init.d/40-post-adjustments/hostfile.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -eu
 
-# Reset hostfile to $AUTH_DEVICE_HOSTKEY
-if [ ! -z ${AUTH_DEVICE_HOSTKEY+x} ]; then
+# Reset hostfile to $OPENHAB_HOSTKEY
+if [ ! -z ${OPENHAB_HOSTKEY+x} ]; then
     FILE=${OPENHAB_HOME}/userdata/etc/host.key
-    echo "hostfile.sh: Adjust $FILE with \$AUTH_DEVICE_HOSTKEY..."
-    echo ${AUTH_DEVICE_HOSTKEY} > $FILE
+    echo "hostfile.sh: Adjust $FILE with \$OPENHAB_HOSTKEY..."
+    echo ${OPENHAB_HOSTKEY} > $FILE
 else
-    echo 'hostfile.sh: $AUTH_DEVICE_HOSTKEY not set!'
+    echo 'hostfile.sh: $OPENHAB_HOSTKEY not set!'
 fi
--- openhab/mounts/etc/cont-init.d/40-post-adjustments/openhabcloud.sh
+++ openhab/mounts/etc/cont-init.d/40-post-adjustments/openhabcloud.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 set -eu
 
-# Reset openhabcloud to $AUTH_OPENHAB_UUID
-if [ ! -z ${AUTH_OPENHAB_UUID+x} ]; then
+if [ ! -z ${OPENHAB_UUID+x} ]; then
     mkdir -p ${OPENHAB_HOME}/userdata
     FILE=${OPENHAB_HOME}/userdata/uuid
-    echo "openhabcloud.sh: Adjust $FILE with \$AUTH_OPENHAB_UUID..."
-    echo ${AUTH_OPENHAB_UUID} > $FILE
+    echo "openhabcloud.sh: Adjust $FILE with \$OPENHAB_UUID..."
+    echo ${OPENHAB_UUID} > $FILE
 else
-    echo 'openhabcloud.sh: $AUTH_OPENHAB_UUID not set!'
+    echo 'openhabcloud.sh: $OPENHAB_UUID not set!'
 fi
 
 
-if [ ! -z ${AUTH_OPENHAB_SECRET+x} ]; then
+if [ ! -z ${OPENHAB_SECRET+x} ]; then
     mkdir -p ${OPENHAB_HOME}/userdata/openhabcloud
     FILE=${OPENHAB_HOME}/userdata/openhabcloud/secret
-    echo "openhabcloud.sh: Adjust $FILE with \$AUTH_OPENHAB_SECRET..."
-    echo ${AUTH_OPENHAB_SECRET} > $FILE
+    echo "openhabcloud.sh: Adjust $FILE with \$OPENHAB_SECRET..."
+    echo ${OPENHAB_SECRET} > $FILE
 else
-    echo 'openhabcloud.sh: $AUTH_OPENHAB_SECRET not set!'
+    echo 'openhabcloud.sh: $OPENHAB_SECRET not set!'
 fi
